67431762314349dc244dcba0eb07598902b518ad
thathyda <thydatha484@gmail.com>
2024-03-16 23:30:06 +0700

    first initailize

---
+++ src/test/java/com/example/taskworkspring/TaskworkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskworkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskworkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
